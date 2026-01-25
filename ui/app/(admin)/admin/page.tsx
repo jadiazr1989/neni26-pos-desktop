aaf31f7f5e6c4eb30f495a3323f409659cda9d25
josediazromero <jadiazr89@gmail.com>
2026-01-25 14:22:33 -0500

    multiple fixed

--- ui/app/(admin)/admin/page.tsx
+++ ui/app/(admin)/admin/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export default function AdminHome() {
   // Por ahora, si solo existe Setup, manda ahí.
   // Cuando tengas dashboard admin real, aquí lo renderizas.
-  redirect("/admin/setup");
+  redirect("/admin/dashboard");
 }
